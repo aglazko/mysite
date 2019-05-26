13aea8c9de86f811a7973be577b6df977f38c66c
Ann <gazmaket@gmail.com>
2019-05-26 21:01:02 +0300

    index page, place list page

--- site_app/urls.py
+++ site_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.place_list, name='place_list'),
+    url(r'^$', views.index, name='index'),
+    url('place_list/',views.place_list, name='place_list')
 ]
--- site_app/views.py
+++ site_app/views.py
@@ -4,9 +4,9 @@ from .models import Flat, House, Room
 # Create your views here.
 
 
+def index(request):
+    return render(request, 'site_app/index.html')
+
+
 def place_list(request):
-    # flats = []
-    # for f in Flat.objects.all():
-    #     flats.append(f)
-    flats = Flat.objects.all()
-    return render(request, 'site_app/place_list.html', {'flats': flats})
+    return render(request, 'site_app/place_list.html')
