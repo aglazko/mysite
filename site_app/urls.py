dc4456008b9aa11d8080bbd6671c1f7627e7a9bf
Oleksii Petrenko <oleksii.petrenko@globallogic.com>
2019-05-27 14:50:34 +0300

    place_list to placements

--- site_app/urls.py
+++ site_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('place_list/', views.placement_list, name='place_list'),
+    path('placements/', views.placement_list, name='place_list'),
     path('rooms/<int:room_id>/', views.room_get, name='room_get'),
     path('houses/<int:house_id>/', views.house_get, name='house_get'),
     path('flats/<int:flat_id>/', views.flat_get, name='flat_get'),
