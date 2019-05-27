da430aa2dc416a3330ea5b3d996931f0d87181b7
Oleksii Petrenko <oleksii.petrenko@globallogic.com>
2019-05-27 21:34:14 +0300

    add widgets

--- site_app/forms.py
+++ site_app/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-
 from . import models
 
 
-FlatForm = forms.modelform_factory(models.Flat, exclude=('is_approved',))
-HouseForm = forms.modelform_factory(models.House, exclude=('is_approved',))
-RoomForm = forms.modelform_factory(models.Room, exclude=('is_approved',))
+FlatForm = forms.modelform_factory(models.Flat, exclude=('is_approved',), widgets={'end_date': forms.SelectDateWidget()})
+HouseForm = forms.modelform_factory(models.House, exclude=('is_approved',), widgets={'end_date': forms.SelectDateWidget()})
+RoomForm = forms.modelform_factory(models.Room, exclude=('is_approved',), widgets={'end_date': forms.SelectDateWidget()})
